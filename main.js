e90293ec1d0c59eed6ffadec1d10bd843be0bbac
Uladzimir Kaniushenka <105815746+inplvce@users.noreply.github.com>
2023-07-29 15:13:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//          Ternar js
+//  if === ?
+//  else === :
+
+/*
+let greeting;
+let time = 10;
+
+greeting = 20 < 12 ? "Good Mo" : "Good Evening";
+console.log(greeting)
+*/
+
+//          Шаблонные строки
+
+let greeting;
+let time = 10;
+
+greeting = time < 10 ? "Good Morning" : "Good Day";
+console.log(greeting)
+
